74a8de673b63c992f9a5629eca09776d5ad7751b
jintao01 <jintao01@macintoshdeMacBook-Pro.local>
2022-01-04 13:25:31 +0800

    Initial commit

--- tests/test_python_project.py
+++ tests/test_python_project.py
@@ -3,7 +3,7 @@ from python_project.math import multiply_two_numbers
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
 
 
 def test_multiply_two_numbers():
